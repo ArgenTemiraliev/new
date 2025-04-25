4674c9c3bf41f357b2a8ca2a6192d5f556f6f44a
ArgenTemiraliev <temiraliev0304@gmail.com>
2025-04-26 04:02:50 +0600

    new

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,73 @@
+'use client'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import logo from '../../../assents/logo.png'
+
+export default function Header() {
+  const [lang, setLang] = useState('ru')
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+
+  const changeLanguage = (selectedLang) => {
+    setLang(selectedLang)
+    setIsDropdownOpen(false)
+    console.log(`Выбран язык: ${selectedLang}`)
+  }
+    
+    
+  const PhoneIcon = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="M6.62 10.79a15.464 15.464 0 006.59 6.59l2.2-2.2a1 1 0 011.05-.24 11.72 11.72 0 003.68.59 1 1 0 011 1v3.5a1 1 0 01-1 1A17.927 17.927 0 013 5a1 1 0 011-1h3.5a1 1 0 011 1 11.72 11.72 0 00.59 3.68 1 1 0 01-.24 1.05l-2.23 2.23z" />
+    </svg>
+  );
+  
+
+  return (
+    <header className="bg-[#319E47] py-4">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center flex-wrap gap-4">
+          
+          {/* Логотип */}
+          <div className="flex-shrink-0">
+            <a href="/">
+              <Image src={logo} alt="Логотип" className="w-28 h-auto" />
+            </a>
+          </div>
+
+          {/* Телефон */}
+          <div className="text-white text-base md:text-lg flex items-center gap-2">
+            <a href="https://wa.me/998712004040" className="flex items-center decoration-[none] gap-1 hover:underline">
+               <PhoneIcon />
+              <span>+998 71 200 40 40</span>
+            </a>
+          </div>
+
+          {/* Язык (выпадающее меню) */}
+          <div className="relative">
+            <button
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              className="text-white text-lg px-2 py-1 rounded hover:underline  text-green-700 font-bold"  >
+              {lang === 'ru' ? '🇷🇺' : '🇺🇿'}
+            </button>
+
+            {isDropdownOpen && (
+              <div className="absolute right-0 mt-2 w-24 bg-white rounded shadow-md z-10">
+                <button
+                  onClick={() => changeLanguage('ru')}
+                  className="w-full px-2 py-1 text-left hover:bg-green-100" > Русский </button>
+                <button
+                  onClick={() => changeLanguage('uz')}
+                  className="w-full px-2 py-1 text-left hover:bg-green-100" >  Ўзбекча </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import Image from 'next/image'
+import place1 from "../../../assents/places1.png";
+import place2 from "../../../assents/places2.png";
+import place3 from "../../../assents/places3.png";
+import place4 from "../../../assents/places4.png";
+import img650 from "../../../assents/image 650.png";
+
+const information = [
+  { name: "Дворец международных форумов Узбекистан", img: place1 },
+  { name: "HUMO Arena", img: place2 },
+  { name: "Дворец Дружбы Народов", img: place3 },
+  { name: "Дворец Творчества Молодежи", img: place4 },
+];
+
+const recommendation = [
+  { name: "HUMO Arena", img: place2 },
+  { name: "HUMO Arena", img: place3 },
+];
+
+function Card({ item }) {
+  return (
+    <div className="bg-white rounded-3xl shadow-xl overflow-hidden pt-4 mt-8 transition hover:scale-[1.01] duration-300">
+      <div className="flex justify-center">
+        <div className="relative w-full max-w-[95%] h-[300px]"> {/* фиксированная высота */}
+          <Image
+            src={item.img}
+            alt={`Фото ${item.name}`}
+            className="w-full h-full object-cover rounded-3xl "
+            priority
+          />
+        </div>
+      </div>
+      <h2 className="text-2xl px-5 py-4 font-semibold">{item.name}</h2>
+      <ul className="flex flex-col sm:flex-row sm:gap-6 px-5">
+        <li className="sm:w-1/3 mb-4">
+          <h3 className="text-lg font-medium">Количество мест</h3>
+          <span className="text-gray-600">3,500</span>
+        </li>
+        <li className="sm:w-2/3 mb-4">
+          <h3 className="text-lg font-medium">Адрес</h3>
+          <span className="text-gray-600">
+            Улица Тоголок Молдо, 40. Первомайский район, Бишкек, 720001.
+          </span>
+        </li>
+      </ul>
+      <div className="px-5 pb-6">
+        <button className="w-full mt-4 px-6 py-3 bg-green-600 text-white font-semibold rounded-3xl hover:bg-green-700 transition duration-300">
+          Купить билеты
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function Hero() {
+  return (
+    <div className="hero py-10 bg-gray-50">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col lg:flex-row gap-10">
+          {/* Left Block */}
+          <div className="lg:w-2/3">
+            <h1 className="text-4xl font-bold mb-6 ml-2">Места</h1>
+            {information.map((item, index) => (
+              <Card key={index} item={item} />
+            ))}
+            <div className="flex justify-center mt-8">
+              <button className="px-6 py-3 bg-green-100 text-green-800 font-semibold rounded-3xl hover:bg-green-600 hover:text-white transition duration-300 shadow-md">
+                Загрузить ещё
+              </button>
+            </div>
+          </div>
+
+          {/* Right Block */}
+          <div className="lg:w-1/3">
+            <Image src={img650} alt="Рекомендации" className="w-full  rounded-xl" />
+            <h2 className="text-3xl font-bold mb-4 ml-2">Рекомендуемые площадки</h2>
+            {recommendation.map((item, index) => (
+              <Card key={index} item={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
