9fa764068f14b0d00f35941ce68825228929f132
ArgenTemiraliev <temiraliev0304@gmail.com>
2025-05-16 12:18:23 +0600

    vercel app

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -5,10 +5,11 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
-// Настройка иконок (иначе не показываются маркеры)
-delete (L.Icon.Default.prototype as any)._getIconUrl
+// Настройка иконок (иначе маркеры не показываются)
+delete L.Icon.Default.prototype._getIconUrl
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png',
+  iconRetinaUrl:
+    'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png',
   iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png',
   shadowUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png',
 })
@@ -20,11 +21,15 @@ export default function Map() {
     setIsClient(true)
   }, [])
 
-  if (!isClient) return null // не рендерим карту на сервере
+  if (!isClient) return null
 
   return (
     <div className="w-full h-[400px] rounded-3xl overflow-hidden">
-      <MapContainer center={[42.8746, 74.5698]} zoom={13} className="w-full h-full z-0">
+      <MapContainer
+        center={[42.8746, 74.5698]}
+        zoom={13}
+        className="w-full h-full z-0"
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
